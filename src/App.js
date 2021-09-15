7d6a7cb38c44e025228a141d8a7f550b4a21a92c
Guilherme da Costa <guilhermedc93@gmail.com>
2021-09-15 18:11:11 -0300

    about session completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from './components/About';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import './css/App.css';
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import perfil from '../images/perfil.png';
+import aboutIcon from '../icons/about-icon.svg';
+import '../css/About.css';
+
+export default function About() {
+ return (
+   <div className="container">
+     <img
+      src={ perfil }
+      alt="foto de perfil de Guilherme"
+      id="perfil-image"
+      />
+      <h2>Quem é <span>Guilherme?</span></h2>
+     <p id="description">
+     Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
+     </p>
+     <img
+      id="about-icon"
+      src={ aboutIcon }
+      alt="Ícone illustrativo da sessão about (6 pontos)"
+     />
+   </div>
+ )
+}
